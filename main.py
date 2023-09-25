6e2fb11d36b5cf29a098ac042521fae713e24f1e
Yuvaraj <144339904+Yuvaramanimaran@users.noreply.github.com>
2023-09-25 23:09:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+Implement a function called sort_student that takes a list of student objects as input and sort the
+list based on their CGPA
+'''
+
+class Student:
+
+def __init__(self, name, roll_number, cgpa): 
+  self.name = name
+  self.roll_number = roll_number 
+  self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # sort the list of students in descending order of CGPA 
+  sorted_students = sorted(student_list,
+key-lambda student: student.cgpa,
+reverse=True)
+return sorted_students
+
+
+# Example usage:
+students = [
+  student("Hari","A123", 7.8),
+  student("Mahidhar","A126",9.9)
+]
+
+sorted_students = sort_students(students)
+
+# print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}". format(student.name,
+                                                     student.rool_number,
+                                                     student.cgpa))
